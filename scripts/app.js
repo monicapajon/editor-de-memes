e67bf32f2ac880a7314f55bded243810320f5f9a
Monica Pajon <moniestudio161@gmail.com>
2022-07-09 16:30:06 -0300

    Se agregó funcionalidad a los botones

--- scripts/app.js
+++ scripts/app.js
@@ -10,3 +10,41 @@ const closeAside = document.getElementById('close-aside')
 const aside = document.querySelector('.aside')
 // ------HEADER
 // --Boton imagen
+btnImgAside.addEventListener("click", () => {
+	aside.classList.remove('hidden')
+	asideImg.classList.remove("hidden")
+	asideTxt.classList.add("hidden")
+
+})
+// --Boton texto
+btnTxtAside.addEventListener("click", () => {
+	aside.classList.remove('hidden')
+	asideImg.classList.add("hidden")
+	asideTxt.classList.remove("hidden")
+})
+// --Boton tema oscuro/claro
+btnTheme.addEventListener("click", () => {
+	body.classList.toggle("dark")
+	body.classList.toggle("light")
+	if (body.className == "dark") {
+		textTheme.innerText = "Modo claro"
+	} else {
+		textTheme.innerText = "Modo oscuro"
+	}
+})
+
+//--Cerrar Aside
+closeAside.addEventListener('click', () => {
+	aside.classList.add('hidden')
+})
+
+// --Boton descarga
+downloadBtn.addEventListener("click", () => {
+	domtoimage.toBlob(contImage).then(function (blob) {
+		window.saveAs(blob, "meme.png")
+	})
+})
+
+if (window.screen.width <= '700') {
+	aside.classList.add('hidden')
+}
